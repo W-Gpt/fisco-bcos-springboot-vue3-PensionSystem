68c12076e2a0e8c5d450ca40f507342a9c3dd749
wrye <12956790+wrye@user.noreply.gitee.com>
2024-05-11 18:21:48 +0800

    更新最新版的结构体

--- contracts/MainContract.sol
+++ contracts/MainContract.sol
@@ -4,38 +4,37 @@ pragma solidity ^0.4.25;
 contract MainContract{
     //个人
     struct PersonalInfo{
-        address personalAddr;
-        string id;
+        uint id;
         uint age;
         string name;
         uint[] laborInfoIndex;
     }
     //社保局
-    struct SocialSecurityDepartment {
+    struct SocialSecDept {
         address socialSecurityAddr;
         string city; // 所在城市
-        uint256 maxInsuranceBase; // 最大保险基数
-        uint256 minInsuranceBase; // 最小保险基数
-        uint personalContributionRate; // 个人缴费比例
-        uint companyContributionRate; // 公司缴费比例
+        uint256 maxBase; // 最大保险基数
+        uint256 minBase; // 最小保险基数
+        uint personalRate; // 个人缴费比例
+        uint companyRate; // 公司缴费比例
     }
     //公司单位
-    struct CompanyInfo{
+    struct Company{
         address companyAddress;
         string city;
         string name;
-        uint companyPayments;
+        uint balance;
     }
     // 劳动信息结构体
-    struct LaborInfo {
-        address personalAddr;
-        uint joiningDate; // 参与工作时间
-        uint  ; // 工资
+    struct LaborInfo{
+        uint id;
+        uint workDate; // 参与工作时间
+        uint salary; // 工资
         address companyAddress;
     }
     //养老保险
     struct PensionAccount {
-        address personalAddr;//个人地址
+        uint id;//个人身份证号
         string city; // 所在城市
         uint personalPayments; // 个人已缴纳
         uint companyPayments; //公司已缴纳
@@ -46,11 +45,12 @@ contract MainContract{
     }
     //转移申请
     struct Application {
-        address personalAddr;// 申请人
+        uint id;// 申请人身份证号
         address fromCompany; // 原城市
         address toCompany; // 目标城市
-        bool isApproved; // 是否批准
-        bool isHasReceived;
+        address fromSocialSecDept; //原社保局
+        address toSocialSecDept; //转入社保局
+        uint status; //审批状态 => 0是保存未提交 1是已提交 2是已经转出 3转入已接收;
     }
     mapping(address=> PersonalInfo) personalInfos;
     mapping(address=> SocialSecurityDepartment) socialSecurityDepartments;
@@ -74,6 +74,8 @@ contract MainContract{
         require(msg.sender == owner, "Only contract owner can call this function.");
         _;
     }
+    //基本构思City结构体 => 三个角色
+    
     
 
 }
