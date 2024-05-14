224e61681c283aaeb16880127b546ed768750f67
wrye <12956790+wrye@user.noreply.gitee.com>
2024-05-14 10:13:08 +0800

    测试连接mybatis plus => mysql

--- WebBackUp/src/main/java/com/ooyyh/top/controller/TestController.java
+++ WebBackUp/src/main/java/com/ooyyh/top/controller/TestController.java
@@ -1,12 +1,25 @@
 package com.ooyyh.top.controller;
 
+import com.ooyyh.top.entity.TestEntity;
+import com.ooyyh.top.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 @CrossOrigin
-@RequestMapping
 public class TestController {
+    @Autowired
+    private TestService testService;
+    @RequestMapping("/test")
+    @ResponseBody
+    public List<TestEntity> test()
+    {
+        return testService.getAllTest();
+    }
 
 }
--- WebBackUp/src/main/java/com/ooyyh/top/service/TestService.java
+++ WebBackUp/src/main/java/com/ooyyh/top/service/TestService.java
@@ -1,8 +1,19 @@
 package com.ooyyh.top.service;
 
+import com.ooyyh.top.dao.TestMapper;
+import com.ooyyh.top.entity.TestEntity;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TestService {
+    @Autowired
+    private TestMapper testMapper;
+
+    public List<TestEntity> getAllTest(){
+        return testMapper.selectList(null);
+    }
 
 }
